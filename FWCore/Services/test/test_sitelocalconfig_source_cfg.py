acb728edf54b8a8c16e289c14e137a97a5653e44
Duong <nhduongvn@gmail.com>
2020-01-14 15:38:23 -0600

    update

--- FWCore/Services/test/test_sitelocalconfig_source_cfg.py
+++ FWCore/Services/test/test_sitelocalconfig_source_cfg.py
@@ -4,7 +4,9 @@ process = cms.Process("TEST")
 #process.source = cms.Source("EmptySource")
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-      '/store/data/Run2016C/DoubleMuon/MINIAOD/02Feb2017-v2/70000/7003FC2E-C6E9-E611-B741-0CC47A7C3610.root'
+      '/store/data/Run2016C/DoubleMuon/MINIAOD/02Feb2017-v2/70000/7003FC2E-C6E9-E611-B741-0CC47A7C3610.root',
+      '/store/data/Run2016C/DoubleMuon/MINIAOD/02Feb2017-v2/70000/2E2F8BF3-C4E9-E611-B125-0025905B8576.root',
+      '/store/data/Run2016C/DoubleMuon/MINIAOD/02Feb2017-v2/70000/74B2A55B-ABE9-E611-9C7B-0CC47A78A456.root'
       )
     )
 
